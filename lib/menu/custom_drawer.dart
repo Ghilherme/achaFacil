70e4061087f07127f2414b28630d3d1dc840dd99
Guilherme Oliveira <g.p.oliveira@hotmail.com>
2021-01-28 00:00:04 -0300

    adicionando features de favoritos

--- lib/menu/custom_drawer.dart
+++ lib/menu/custom_drawer.dart
@@ -1,33 +1,57 @@
+import 'package:AchaFacil/screens_lists/favorites_list.dart';
 import 'package:flutter/material.dart';
 import 'package:AchaFacil/login/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../constants.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
     return Drawer(
         child: Column(
       children: [
         UserAccountsDrawerHeader(
             accountName: Text(mainTitleApp),
-            accountEmail: Text('admin@achafacil.com')),
+            accountEmail: Text('Tudo em 3 cliques ou menos!')),
         ListTile(
           leading: Icon(Icons.home),
           enabled: false,
           title: Text('Perfil'),
           subtitle: Text('Meu perfil'),
-          onTap: () {
-            print('tapeou perfil');
-          },
+          onTap: () {},
         ),
         ListTile(
           leading: Icon(Icons.favorite),
           title: Text('Favoritos'),
-          enabled: false,
           subtitle: Text('Meus contatos preferidos'),
-          onTap: () {
-            print('tapeou ordens');
+          onTap: () async {
+            final SharedPreferences prefs = await _prefs;
+            if (prefs.getStringList('favoritos').isEmpty)
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('Ops...'),
+                    content: Text('Adicione um contato aos favoritos antes!'),
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text('Ok'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            else
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => FavoritesList(
+                        idDates: prefs.getStringList('favoritos'),
+                      )));
           },
         ),
         ListTile(
---
+++ lib/screens_lists/favorites_list.dart
@@ -0,0 +1,163 @@
+import 'package:AchaFacil/apis/models/contacts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:AchaFacil/screens_details/body_contact_details.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../constants.dart';
+
+class FavoritesList extends StatelessWidget {
+  FavoritesList({Key key, @required this.idDates}) : super(key: key);
+  final List<String> idDates;
+
+  @override
+  Widget build(BuildContext context) {
+    List<DateTime> dt = idDates.map((e) => DateTime.parse(e)).toList();
+
+    Query query = FirebaseFirestore.instance
+        .collection('contatos')
+        .where('criacao', whereIn: dt)
+        .orderBy('avaliacao.geral', descending: true);
+
+    return Scaffold(
+        appBar: AppBar(
+            title: Text('Favoritos S2'),
+            elevation: 0,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )),
+        body: StreamBuilder(
+          stream: query.snapshots(),
+          builder: (context, stream) {
+            //Trata Load
+            if (stream.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            //Trata Erro
+            if (stream.hasError) {
+              return Center(child: Text(stream.error.toString()));
+            }
+
+            QuerySnapshot querySnapshot = stream.data;
+            var contatos = _orderContactsByDistance(querySnapshot.docs);
+
+            return ListView.builder(
+                padding: EdgeInsets.all(kDefaultPaddingListView),
+                itemCount: contatos.length,
+                itemBuilder: (context, i) {
+                  return _buildRow(context, contatos, i, contatos.length);
+                });
+          },
+        ));
+  }
+
+  String _distanceBetweenKms(double startLat, startLon, endLat, endLon) {
+    return (Geolocator.distanceBetween(startLat, startLon, endLat, endLon)
+                .floor() /
+            1000)
+        .toStringAsFixed(1);
+  }
+
+  Widget _buildRow(BuildContext context, List<DistanceContact> contacts,
+      int indice, int size) {
+    ContactsModel contact = contacts.elementAt(indice).contact;
+    String kms = contacts.elementAt(indice).distance.toString();
+    return Column(children: <Widget>[
+      ListTile(
+          //isThreeLine: true,
+          leading: CircleAvatar(
+              radius: 25,
+              backgroundImage:
+                  contact.imageAvatar == '' || contact.imageAvatar == null
+                      ? AssetImage('assets/images/contacts.jpeg')
+                      : Image.network(contact.imageAvatar).image),
+          subtitle: contact.rating.general == 0
+              ? Text('Seja o primeiro a avaliar!')
+              : Text(
+                  "Atendimento: " +
+                      contact.rating.attendance.toStringAsFixed(1) +
+                      '\n' +
+                      'Qualidade: ' +
+                      contact.rating.quality.toStringAsFixed(1) +
+                      '\n' +
+                      'Preço: ' +
+                      contact.rating.price.toStringAsFixed(1),
+                  style: TextStyle(fontSize: 13.0, color: Colors.grey),
+                ),
+          title: Text(
+            contact.name,
+            style: TextStyle(fontWeight: FontWeight.w500),
+          ),
+          trailing: RichText(
+            textAlign: TextAlign.right,
+            text: TextSpan(
+              style: TextStyle(color: Colors.black),
+              children: [
+                TextSpan(
+                  text: contact.rating.general == 0
+                      ? '--'
+                      : contact.rating.general.toStringAsFixed(1),
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+                ),
+                TextSpan(text: ' '),
+                WidgetSpan(
+                  child: Icon(
+                    Icons.star,
+                    color: contact.rating.general == 0
+                        ? Colors.grey
+                        : Colors.amber,
+                    size: 20,
+                  ),
+                ),
+                TextSpan(text: '\n'),
+                TextSpan(
+                  text: kms.isEmpty ? '' : kms + ' kms',
+                  style: TextStyle(fontSize: 11, color: kTextLightColor),
+                ),
+              ],
+            ),
+          ),
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => BodyContactDetails(
+                      contact: contact,
+                    )));
+          }),
+      indice + 1 == size ? Container() : Divider()
+    ]);
+  }
+
+  List<DistanceContact> _orderContactsByDistance(
+      List<QueryDocumentSnapshot> querySnapshots) {
+    Position _currentPosition = globalPosition;
+
+    List<DistanceContact> distanceContacts = List<DistanceContact>();
+    for (var querySnapshot in querySnapshots) {
+      ContactsModel contact = ContactsModel.fromFirestore(querySnapshot);
+      var distance =
+          contact.address.coordinates == null || _currentPosition == null
+              ? 0.0
+              : double.parse(_distanceBetweenKms(
+                  _currentPosition.latitude,
+                  _currentPosition.longitude,
+                  contact.address.coordinates.latitude,
+                  contact.address.coordinates.longitude));
+
+      distanceContacts
+          .add(DistanceContact(distance: distance, contact: contact));
+    }
+    distanceContacts.sort((a, b) => a.distance.compareTo(b.distance));
+
+    return distanceContacts;
+  }
+}
+
+class DistanceContact {
+  DistanceContact({this.distance, this.contact});
+  double distance;
+  ContactsModel contact;
+}
